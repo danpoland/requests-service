588dce9bffc28ac8a105f9ac805b4483da0c31e2
Dan Poland <danpoland84@gmail.com>
2018-08-02 12:29:34 -0400

    move from factory class to factory method

--- requestsservice/client.py
+++ requestsservice/client.py
@@ -1,14 +1,88 @@
 import requests
 
-from .exceptions import ServiceException
-from .sessions import SessionCache
+__all__ = [
+    'HttpClientException',
+    'HttpClient',
+]
 
 
-class HttpService:
+class HttpClientException(Exception):
     """
-    HTTP inter-process communication service.
-    Wraps the construction of Requests Request objects and returns Response instances.
+    Basic exception for wrapping Requests Responses into throwable exceptions when the response.status_code is >= 300.
     """
+
+    def __init__(self, response):
+        """
+        :param response: Requests Response object
+        """
+
+        self.response = response
+
+
+class SessionCache:
+    """
+    Simple caching mechanism for Request Sessions, allows us to keep the Session connection pools open.
+    """
+
+    __sessions = {}
+
+    @classmethod
+    def get(cls, name):
+        return cls.__sessions.get(name)
+
+    @classmethod
+    def set(cls, name, session):
+        cls.__sessions[name] = session
+
+
+class HttpClient:
+    """
+    Http client used to consume HTTP APIs.
+    Wraps the construction of Requests ``Request`` objects and returns Response instances.
+
+    Factory Attributes:
+        _service_root     The absolute URL path to the service being accessed.
+        _default_headers  A dictionary of default headers to be persisted on the Session used to make requests.
+        _timeout          A default timeout int or tuple (connect, read) to be used on every request.
+        _max_retires      The maximum number of retries allowed before a request fails.
+        _auth             The default AuthBase callable to be used with every request.
+    """
+
+    _service_root = ''
+    _default_headers = {}
+    _timeout = (3, 10)
+    _max_retires = 3
+    _auth = None
+
+    @classmethod
+    def make_client(cls, auth_kwargs=None):
+        """
+        Factory method for creating a new instance of the client.
+
+        :param auth_kwargs: dict of kwargs that will be passed to the AuthBase callable used by the factory.
+                            The auth is not used by the session but is used by every request made by the HttpService
+                            instance (auth is not persisted with the session).
+        :return: HTTPClient object.
+        """
+
+        session = SessionCache.get(cls.__name__)
+
+        if not session:
+            adapter = requests.adapters.HTTPAdapter(max_retries=cls._max_retires)
+            session = requests.Session()
+
+            session.mount('http://', adapter=adapter)
+            session.mount('https://', adapter=adapter)
+            session.headers.update(cls._default_headers)
+
+            SessionCache.set(cls.__name__, session)
+
+        auth = cls._auth
+
+        if auth:
+            auth = auth(**auth_kwargs) if auth_kwargs else auth()
+
+        return cls(cls._service_root, session, auth)
 
     def __init__(self, url_root, session=None, auth=None, timeout=None):
         """
@@ -34,9 +108,7 @@ class HttpService:
         Example:
             path = '/customer/{customer_id}/
             path_params = {'customer_id': 'c1'}
-
             print(_build_url(path, path_params)
-
             >> '/customer/c1/'
         """
 
@@ -98,11 +170,9 @@ class HttpService:
         )
 
         if res.status_code >= 300 and raise_exception:
-            raise ServiceException(response=res)
+            raise HttpClientException(response=res)
 
         return res
-
-    # ------ Shortcut methods ------
 
     def get(self, path='', path_params=None, query_params=None, headers=None, raise_exception=True):
         return self.make_request('GET', path, path_params, query_params=query_params,
@@ -123,52 +193,3 @@ class HttpService:
     def delete(self, path='', path_params=None, headers=None, raise_exception=True):
         return self.make_request('DELETE', path, path_params,
                                  headers=headers, raise_exception=raise_exception)
-
-
-class HttpServiceFactory:
-    """
-    Base factory class for constructing HTTPServices for services with HTTP interfaces.
-    Utilizes Requests Session to persist default values between requests and to take advantage
-    of connection pooling.
-
-    Attributes:
-        service_root        The absolute URL path to the service being accessed.
-        default_headers     A dictionary of default headers to be persisted on the Session used to make requests.
-        timeout             A default timeout int or tuple (connect, read) to be used on every request.
-        max_retires         The maximum number of retries allowed before a request fails.
-        auth                The default AuthBase callable to be used with every request.
-    """
-
-    service_root = ''
-    default_headers = {}
-    timeout = (3, 10)
-    max_retires = 3
-    auth = None
-
-    @classmethod
-    def make_service(cls, auth_kwargs=None):
-        """
-        :param auth_kwargs: dict of kwargs that will be passed to the AuthBase callable used by the factory.
-                            The auth is not used by the session but is used by every request made by the HttpService
-                            instance (auth is not persisted with the session).
-        :return: HTTPService object.
-        """
-
-        session = SessionCache.get(cls.__name__)
-
-        if not session:
-            adapter = requests.adapters.HTTPAdapter(max_retries=cls.max_retires)
-            session = requests.Session()
-
-            session.mount('http://', adapter=adapter)
-            session.mount('https://', adapter=adapter)
-            session.headers.update(cls.default_headers)
-
-            SessionCache.set(cls.__name__, session)
-
-        auth = cls.auth
-
-        if auth:
-            auth = auth(**auth_kwargs) if auth_kwargs else auth()
-
-        return HttpService(cls.service_root, session, auth)
